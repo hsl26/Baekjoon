38ccc50bc2739ed5b76b5e9c9e44ecd6eeb17dd4
HS Lee <100758335+hsl26@users.noreply.github.com>
2024-06-26 21:18:56 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -0,0 +1,10 @@
+word = input()
+answer = []
+
+for i in range(len(word)):
+    if word[i].isupper():
+        answer.append(word[i].lower())
+    else:
+        answer.append(word[i].upper())
+
+print(''.join(answer))
