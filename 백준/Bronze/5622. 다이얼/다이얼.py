14a1411883231b4bab8bfef8382f0de1bacb8922
HS Lee <100758335+hsl26@users.noreply.github.com>
2024-09-07 22:24:52 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,20 +1,12 @@
-dial = {2: ['A', 'B', 'C'],
-        3: ['D', 'E', 'F'],
-        4: ['G', 'H', 'I'],
-        5: ['J', 'K', 'L'],
-        6: ['M', 'N', 'O'],
-        7: ['P', 'Q', 'R', 'S'],
-        8: ['T', 'U', 'V'],
-        9: ['W', 'X', 'Y', 'Z']
-        }
+dial = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
 
 string_number = input()
 
 spend_time = 0
 for sn in string_number:
-    for key in dial:
-        if sn in dial[key]:
-            time = key + 1
+    for idx, alphas  in enumerate(dial):
+        if sn in alphas:
+            time = idx + 3
             spend_time += time
             break
 
