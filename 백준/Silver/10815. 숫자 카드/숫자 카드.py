082a1ae9af0bfd724f9760edeae38e298be48cd7
HS Lee <100758335+hsl26@users.noreply.github.com>
2024-09-24 22:18:21 +0900

    [Silver V] Title: 숫자 카드, Time: 892 ms, Memory: 133796 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,16 @@
+N = int(input())
+have = list(map(int, input().split()))
+M, m = max(have), min(have)
+
+card = {}
+for h in have:
+    card[h] = 1
+
+M = int(input())
+is_have = list(map(int, input().split()))
+
+for i in is_have:
+    try:
+        print(card[i], end=' ')
+    except:
+        print(0, end=' ')
