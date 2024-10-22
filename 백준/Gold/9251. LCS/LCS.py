d5b7c09ecf76daee061280c72603692a9ddee879
HS Lee <100758335+hsl26@users.noreply.github.com>
2024-10-22 15:45:23 +0900

    [Gold V] Title: LCS, Time: 540 ms, Memory: 38300 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -7,19 +7,24 @@ if A[0] == B[0]:
 
 def initialize():
     for a in range(1, len(A)):
-        DP[a][0] = max(1, DP[a-1][0]) if A[a] == B[0] else DP[a-1][0]
+        DP[a][0] = DP[a - 1][0]
+        if A[a] == B[0]:
+            DP[a][0] = 1
 
-    
+
     for b in range(1, len(B)):
-        DP[0][b] = max(1, DP[0][b-1]) if A[0] == B[b] else DP[0][b-1]
+        DP[0][b] = DP[0][b-1]
+        if A[0] == B[b]:
+            DP[0][b] = 1
 
 initialize()
+
 
 for a in range(1, len(A)):
     for b in range(1, len(B)):
         if A[a] == B[b]:
-            DP[a][b] = max(DP[a-1][b-1]+1, DP[a-1][b], DP[a][b-1])
+            DP[a][b] = max(DP[a][b-1], DP[a-1][b], DP[a-1][b-1]+1)
         else:
-            DP[a][b] = max(DP[a-1][b], DP[a][b-1])
-        
+            DP[a][b] = max(DP[a][b-1], DP[a-1][b])
+
 print(DP[len(A)-1][len(B)-1])
