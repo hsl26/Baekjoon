c5249a4122250996639774ed3f6ec367577ec764
HS Lee <100758335+hsl26@users.noreply.github.com>
2024-10-31 17:07:13 +0900

    [Gold V] Title: A와 B, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -5,10 +5,8 @@ T = list(input())
 # 맨 뒤가 B면 pop 후 문자열 뒤집기
 ans = 0
 while T:
-    if T[-1] == 'A':
-        T.pop()
-    else:
-        T.pop()
+    last = T.pop()
+    if last == 'B':
         T.reverse()
     if T == S:
         ans = 1
