b20566ac4fa9a6c7bc223bb0c2a377995bbe6393
HS Lee <100758335+hsl26@users.noreply.github.com>
2024-11-27 14:13:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 2868 ms, Memory: 123072 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -6,23 +6,23 @@ V, E = map(int, input().split())
 arr = []
 for _ in range(E):
     A, B, C = map(int, input().split())
-    arr.append([C, A-1, B-1])
+    arr.append([A-1, B-1, C])
 
-arr.sort()
+arr.sort(key=lambda x: x[2])
 
 DS = [i for i in range(V)]
 
 def find_set(a):
     while a != DS[a]:
         a = DS[a]
-    return a
+    return DS[a]
 
 MST = 0
-for i in arr:
-    C, A, B = i
+for i in range(E):
+    A, B, C = arr[i]
     ap = find_set(A)
     bp = find_set(B)
-
+    
     if ap != bp:
         DS[max(ap, bp)] = min(ap, bp)
         MST += C
