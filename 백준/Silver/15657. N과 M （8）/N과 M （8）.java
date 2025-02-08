53853f50bf554501149a8a4de9222f2df9e62240
HS Lee <100758335+hsl26@users.noreply.github.com>
2025-02-08 23:14:10 +0900

    [Silver III] Title: N과 M (8), Time: 248 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static BufferedWriter bw;
+	static int N, M;
+	static int[] Ns;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] arr = br.readLine().split(" ");
+		N = Integer.parseInt(arr[0]);
+		M = Integer.parseInt(arr[1]);
+
+		Ns = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		Arrays.sort(Ns);
+
+		bt(0, new ArrayList<>());
+
+		bw.flush();
+		bw.close();
+	}
+
+	private static void bt(int d, ArrayList<Integer> a) throws IOException {
+		if (a.size() == M) {
+			for (int n : a)
+				bw.write(n + " ");
+			bw.write("\n");
+			return;
+		}
+
+		for (int i = d; i < N; i++) {
+			a.add(Ns[i]);
+			bt(i, a);
+			a.remove(a.size() - 1);
+		}
+	}
+
+}
