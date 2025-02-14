f968cf0a30a315b1f334ffda80a1980d299775b8
HS Lee <100758335+hsl26@users.noreply.github.com>
2025-02-15 01:17:51 +0900

    [Silver V] Title: 단어 정렬, Time: 260 ms, Memory: 31744 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,26 +1,43 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int N = Integer.parseInt(br.readLine());
-		
-		Set<String> wordSet = new TreeSet<>((s1, s2) -> {
-			return s1.length() == s2.length() ? s1.compareTo(s2) : Integer.compare(s1.length(), s2.length());
+
+		String[] arr = new String[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = br.readLine();
+		}
+
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				if (s1.length() == s2.length()) {
+					for (int i = 0; i < s1.length(); i++) {
+						if (s1.charAt(i) != s2.charAt(i))
+							return s1.charAt(i) - s2.charAt(i);
+					}
+				}
+				return s1.length() - s2.length();
+			}
 		});
-		
-		for (int i=0; i<N; i++) {
-			wordSet.add(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			if (i != 0 && arr[i].equals(arr[i-1])) continue;
+			bw.write(arr[i] + "\n");
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		for (String word : wordSet) {
-			sb.append(word).append("\n");
-		}
-		System.out.print(sb.toString());
+
+		bw.flush();
+		bw.close();
+
 	}
 
 }
