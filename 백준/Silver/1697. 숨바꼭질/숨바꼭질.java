091e58f35b54df94dfc69c0b7a29854a18e67c43
HS Lee <100758335+hsl26@users.noreply.github.com>
2025-02-21 14:14:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 21924 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,67 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
 
-  static Queue<Integer> q;
-  static int[] visit;
-  static int[] cost;
-
   static int N, K;
 
-  public static void main(String[] args)  {
+  static int[] load, visited;
 
-    Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws IOException {
 
-    N = sc.nextInt();
-    K = sc.nextInt();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    visit = new int[200000 + 1];
-    cost = new int[200000 + 1];
+    String[] arr = br.readLine().split(" ");
+    N = Integer.parseInt(arr[0]);
+    K = Integer.parseInt(arr[1]);
 
-    q = new LinkedList<>();
+    load = new int[2 * Math.max(N, K) + 1];
+    Arrays.fill(load, -1);
+    visited = new int[2 * Math.max(N, K) + 1];
 
-    q.add(N);
-    visit[N] = 1; // 방문한게 1
-    dfs();
+    bfs();
 
-    System.out.println(cost[K]);
+    System.out.println(load[K]);
 
   }
 
-  private static void dfs() {
+  private static void bfs() {
+
+    Queue<Integer> q = new LinkedList<>();
+
+    q.add(N);
+    load[N] = 0;
 
     while (!q.isEmpty()) {
-      int a = q.remove();
-      int n = a;
+      int n = q.remove();
 
-      for (int i = 0; i < 3; i++) {
-        if (i == 0)
-          n = a - 1;
-        else if (i == 1)
-          n = a + 1;
-        else
-          n = a * 2;
 
-        if (can_go(n)) {
-          q.add(n);
-          visit[n] = 1;
-          cost[n] = cost[a] + 1;
+      for (int d = 0; d < 3; d++) {
+        int nn;
+        if (d == 0) {
+          nn = n - 1;
+          if (nn < 0 || nn >= load.length || load[nn] >= 0)
+            continue;
+        } else if (d == 1) {
+          nn = n + 1;
+          if (nn < 0 || nn >= load.length || load[nn] >= 0)
+            continue;
+        } else {
+          nn = n * 2;
+          if (nn < 0 || nn >= load.length || load[nn] >= 0)
+            continue;
         }
 
+        load[nn] = load[n] + 1;
+        q.add(nn);
       }
     }
 
-  }
-
-  private static boolean can_go(int n) {
-
-    if (n < 0 || n > 200000)
-      return false;
-    if (visit[n] == 1)
-      return false;
-
-    return true;
   }
 }
