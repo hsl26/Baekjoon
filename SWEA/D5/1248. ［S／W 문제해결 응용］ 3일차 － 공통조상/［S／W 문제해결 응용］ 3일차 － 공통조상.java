6c5b04d4cfe0f61295c31f82deac39197932d875
HS Lee <100758335+hsl26@users.noreply.github.com>
2025-02-25 13:51:06 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 151 ms, Memory: 40,180 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -1,74 +1,118 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int[] parent;
-	static boolean[] visited;
-	static int cnt;
+  static int V, E;
 
-	static StringBuilder sb = new StringBuilder();
+  static Map<Integer, List<Integer>> TM;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int[] parent;
+  static int[] level;
 
-		int T = Integer.parseInt(br.readLine());
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
 
-		for (int t = 1; t <= T; t++) {
-			sb.append("#" + t + " ");
+    int T = Integer.parseInt(br.readLine());
 
-			String[] arr = br.readLine().split(" ");
-			int V = Integer.parseInt(arr[0]);
-			int E = Integer.parseInt(arr[1]);
-			int v1 = Integer.parseInt(arr[2]);
-			int v2 = Integer.parseInt(arr[3]);
+    for (int t = 1; t <= T; t++) {
+      sb.append("#").append(t).append(" ");
 
-			parent = new int[V + 1];
-			visited = new boolean[V + 1];
+      StringTokenizer st = new StringTokenizer(br.readLine());
 
-			String[] vs = br.readLine().split(" ");
+      V = Integer.parseInt(st.nextToken());
+      E = Integer.parseInt(st.nextToken());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
 
-			for (int i = 0; i < E; i++) {
-				parent[Integer.parseInt(vs[2 * i + 1])] = Integer.parseInt(vs[2 * i]);
-			}
-			
-//			공통 조상 찾기  
-			Set<Integer> ancestors = new HashSet<>();
-			while (v1 != 0) {  
-			    ancestors.add(v1);
-			    v1 = parent[v1];
-			}
-			while (v2 != 0) { 
-			    if (ancestors.contains(v2)) break;
-			    v2 = parent[v2];
-			}
-			sb.append(v2 + " ");
+      parent = new int[V + 1];
+      level = new int[V + 1];
+      TM = new HashMap<>();
 
-//			자식 개수 찾기 
-			cnt = 0;
-			subs(v2);
-			sb.append(cnt);
+      parent[1] = 1;
+      level[1] = 1;
 
-			sb.append("\n");
-		}
-		System.out.println(sb);
-	}
+      for (int v = 1; v <= V; v++) {
+        TM.put(v, new ArrayList<>());
+      }
 
-	private static void subs(int sp) {
-		Queue<Integer> q = new LinkedList<>();
-		visited[sp] = true;
-		q.add(sp);
-		while (!q.isEmpty()) {
-			int cur = q.remove();
-			cnt++;
+      st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < E; i++) {
+        int p = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-			for (int p = 1; p < parent.length; p++) {
-				if (parent[p] == cur) {
-					q.add(p);
-				}
-			}
-		}
+        TM.get(p).add(c);
+      }
 
-	}
+      setTree(1, 0);
 
+      int lca = LCA(a, b);
+      int size = subtreeSize(lca);
+
+      sb.append(lca).append(" ").append(size).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+  private static int subtreeSize(int lca) {
+    int cnt = 1;
+
+    Queue<Integer> q = new LinkedList<>();
+    int[] visited = new int[V + 1];
+
+    q.add(lca);
+    visited[lca] = 1;
+
+    while (!q.isEmpty()) {
+      int n = q.remove();
+      cnt += TM.get(n).size();
+
+      for (int i = 0; i < TM.get(n).size(); i++) {
+        int nn = TM.get(n).get(i);
+        if (visited[nn] == 0)
+          q.add(nn);
+      }
+    }
+
+    return cnt;
+  }
+
+  private static int LCA(int a, int b) {
+    // 깊이를 일치시킴
+    while (level[a] > level[b])
+      a = parent[a];
+    while (level[a] < level[b])
+      b = parent[b];
+
+    // 부모가 같아질 때까지 거슬러 올라감
+    while (a != b) {
+      a = parent[a];
+      b = parent[b];
+    }
+
+    return a;
+  }
+
+  private static void setTree(int c, int p) {
+
+    parent[c] = p;
+    level[c] = level[p] + 1;
+
+    for (int i = 0; i < TM.get(c).size(); i++) {
+      if (c == TM.get(c).get(i))
+        continue;
+
+      setTree(TM.get(c).get(i), c);
+    }
+
+  }
 }
