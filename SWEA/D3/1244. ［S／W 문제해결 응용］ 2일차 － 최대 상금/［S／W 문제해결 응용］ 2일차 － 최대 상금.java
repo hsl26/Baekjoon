83bcb4fa58722c48da595bb5c78f7bcad5de1fa9
HS Lee <100758335+hsl26@users.noreply.github.com>
2025-03-11 11:45:34 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 8,665 ms, Memory: 27,776 KB -BaekjoonHub

---
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Solution {
+  static int N;
+  static int max = 0;
+  static int[] arr;
+
+  public static void main(String args[]) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int T;
+    T = sc.nextInt();
+
+    for (int test_case = 1; test_case <= T; test_case++) {
+      String s = sc.next();
+      N = sc.nextInt();
+      arr = new int[s.length()];
+
+      for (int i = 0; i < arr.length; i++) {
+        arr[i] = s.charAt(i) - '0';
+      }
+
+      if (arr.length < N) {
+        N = arr.length;
+      }
+
+      DFS(0, 0);
+
+      System.out.println("#" + test_case + " " + max);
+      max = 0;
+    }
+  }
+
+  static void DFS(int start, int depth) {
+    if (depth == N) {
+      int result = 0;
+      for (int i = 0; i < arr.length; i++) {
+        result += (Math.pow(10, i) * arr[arr.length - i - 1]);
+      }
+
+      max = Math.max(result, max);
+
+      return;
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        swap(i, j);
+        DFS(i, depth + 1);
+        swap(i, j);
+      }
+    }
+  }
+
+  static void swap(int i, int j) {
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+}
