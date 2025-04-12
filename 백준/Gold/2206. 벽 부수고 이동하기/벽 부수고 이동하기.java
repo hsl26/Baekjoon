16b9804cf9adf3baa8f493b9eb13c4d5d40c9046
HS Lee <100758335+hsl26@users.noreply.github.com>
2025-04-12 20:14:25 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1140 ms, Memory: 207396 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,77 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  static final int MAX = 1000 * 1000 + 1;
   static int N, M;
-
-  static int[] dr = {0, 0, 1, -1};
-  static int[] dc = {1, -1, 0, 0};
-
-  static int[][] board;
-  static int[][][] dist;
+  static int[] drs = {0, 0, 1, -1}, dcs = {1, -1, 0, 0};
+  static int[][] map;
+  static int[][][] distance;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
 
-    board = new int[N][M];
-    dist = new int[N][M][2];
+    map = new int[N][M];
+    distance = new int[N][M][2];
 
     for (int r = 0; r < N; r++) {
       String[] arr = br.readLine().split("");
       for (int c = 0; c < M; c++) {
-        board[r][c] = Integer.parseInt(arr[c]);
-        dist[r][c][0] = dist[r][c][1] = MAX;
+        map[r][c] = Integer.parseInt(arr[c]);
+        Arrays.fill(distance[r][c], 1000001);
       }
     }
 
-    int answer = bfs();
+    bfs();
 
-    System.out.println(answer);
+    int ans = 0;
+    if (distance[0][0][0] == 1000001 && distance[0][0][1] == 1000001)
+      ans = -1;
+    else
+      ans = Math.min(distance[0][0][0], distance[0][0][1]);
+
+    System.out.println(ans);
   }
 
-  private static int bfs() {
-    Queue<int[]> q = new LinkedList<>();
+  private static void bfs() {
+    int r, c, nr, nc;
+    int[] arr = new int[2];
 
-    q.add(new int[] {0, 0, 0});
-    dist[0][0][0] = 1;
+    Queue<int[]> q = new LinkedList<>();
+    distance[N - 1][M - 1][0] = 1;
+    q.add(new int[] {N - 1, M - 1});
 
     while (!q.isEmpty()) {
-      int[] cur = q.poll();
-      int r = cur[0], c = cur[1], w = cur[2];
-
-      if (r == N - 1 && c == M - 1)
-        return dist[r][c][w];
+      arr = q.remove();
+      r = arr[0];
+      c = arr[1];
 
       for (int d = 0; d < 4; d++) {
-        int nr = r + dr[d];
-        int nc = c + dc[d];
+        nr = r + drs[d];
+        nc = c + dcs[d];
 
         if (nr < 0 || nr >= N || nc < 0 || nc >= M)
           continue;
 
-        if (board[nr][nc] == 0 && dist[nr][nc][w] > dist[r][c][w] + 1) {
-          dist[nr][nc][w] = dist[r][c][w] + 1;
-          q.add(new int[] {nr, nc, w});
+        if (map[nr][nc] == 1) {
+          if (distance[r][c][0] + 1 < distance[nr][nc][1]) {
+            distance[nr][nc][1] = distance[r][c][0] + 1;
+            q.add(new int[] {nr, nc});
+          }
+        } else {
+          if (distance[r][c][0] + 1 < distance[nr][nc][0]
+              || distance[r][c][1] + 1 < distance[nr][nc][1]) {
+            q.add(new int[] {nr, nc});
+          }
+          if (distance[r][c][0] + 1 < distance[nr][nc][0])
+            distance[nr][nc][0] = distance[r][c][0] + 1;
+          if (distance[r][c][1] + 1 < distance[nr][nc][1])
+            distance[nr][nc][1] = distance[r][c][1] + 1;
+
+
         }
 
-        if (board[nr][nc] == 1 && w == 0 && dist[nr][nc][1] > dist[r][c][w] + 1) {
-          dist[nr][nc][1] = dist[r][c][w] + 1;
-          q.add(new int[] {nr, nc, 1});
-        }
       }
     }
-
-    return -1;
   }
+
 }
