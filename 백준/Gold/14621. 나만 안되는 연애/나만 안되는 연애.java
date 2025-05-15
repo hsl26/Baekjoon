6e6be34833544c13ae882d734a640757245c4bb4
HS Lee <100758335+hsl26@users.noreply.github.com>
2025-05-15 15:21:27 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 264 ms, Memory: 21724 KB -BaekjoonHub

---
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N, M, A, B, C;
+  static int[] parent;
+  static PriorityQueue<int[]> line = new PriorityQueue<>((l1, l2) -> {
+    return l1[2] - l2[2];
+  });
+
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    String[] s = br.readLine().split(" ");
+
+    parent = new int[N + 1];
+
+    for (int i = 1; i <= N; i++) {
+      parent[i] = i;
+    }
+
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      A = Integer.parseInt(st.nextToken());
+      B = Integer.parseInt(st.nextToken());
+      C = Integer.parseInt(st.nextToken());
+
+      if (!s[A - 1].equals(s[B - 1]))
+        line.add(new int[] {A, B, C});
+
+    }
+
+    int sum = mst();
+
+    System.out.println(sum);
+  }
+
+
+  private static int mst() {
+    int sum = 0, cnt = 0;
+
+    int n = line.size();
+
+    for (int i = 0; i < n; i++) {
+      if (cnt == N - 1)
+        break;
+
+      int[] arr = line.poll();
+
+      int p1 = find(arr[0]);
+      int p2 = find(arr[1]);
+
+      if (p1 == p2) {
+        continue;
+      } else {
+        union(p1, p2);
+        cnt++;
+        sum += arr[2];
+      }
+
+    }
+
+    if (cnt != N - 1)
+      sum = -1;
+
+    return sum;
+  }
+
+  private static void union(int a, int b) {
+
+    if (a < b)
+      parent[b] = a;
+    else
+      parent[a] = b;
+
+  }
+
+  private static int find(int n) {
+
+    if (parent[n] == n)
+      return n;
+
+    return parent[n] = find(parent[n]);
+  }
+
+
+}
