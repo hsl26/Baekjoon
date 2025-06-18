ebae6074573ecc820a52926a16ed6fef56cd60a6
HS Lee <100758335+hsl26@users.noreply.github.com>
2025-06-18 13:09:35 +0900

    [Gold IV] Title: 동전 1, Time: 76 ms, Memory: 11816 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,33 +1,32 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  static int n, k;
+  static int N, K;
 
-  public static void main(String[] args) throws IOException {
+  static int[] num, dp;
+
+  public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     StringTokenizer st = new StringTokenizer(br.readLine());
 
-    n = Integer.parseInt(st.nextToken());
-    k = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
 
-    int[] arr = new int[n + 1];
-    int[] dp = new int[k + 1];
+    num = new int[N + 1];
+    dp = new int[K + 1];
     dp[0] = 1;
 
-    for (int i = 1; i <= n; i++) {
-      arr[i] = Integer.parseInt(br.readLine());
-      for (int j = arr[i]; j <= k; j++) {
-        dp[j] += dp[j - arr[i]];
+    for (int i = 1; i <= N; i++) {
+      num[i] = Integer.parseInt(br.readLine());
+      for (int j = num[i]; j <= K; j++) {
+        dp[j] += dp[j - num[i]];
       }
     }
 
-    System.out.println(dp[k]);
-
+    System.out.println(dp[K]);
   }
 
 }
